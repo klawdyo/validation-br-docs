f1491aa67ccfbe2648611c43cff609ebe77694b2
Claudio Medeiros <klawdyo@gmail.com>
2026-01-01 12:18:34 -0300

    fix: Inclui validador de exemplo no cpf

---
+++ src/components/validator/validator.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="vp-doc">
+    <div class="input-group">
+      <input v-model="modelValue" type="text" :placeholder="placeholder" class="vp-input" />
+    </div>
+
+    <template v-if="modelValue">
+      <p v-if="isValid" class="success-message">
+        {{ successMessage }}
+      </p>
+      <p v-else-if="successMessage" class="error-message">
+        {{ errorMessage }}
+      </p>
+    </template>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { computed, ref, watch } from 'vue';
+
+const props = defineProps<{
+  placeholder: string;
+  successMessage: string;
+  errorMessage: string;
+  handle: () => boolean;
+}>();
+
+const modelValue = defineModel<string | undefined>();
+const isValid = computed(props.handle)
+
+
+</script>
